bfa9033eefdeb7c3d1422469e1af5103e92d8fd0
Mahan Jadiyavar <jdmahan69@gmail.com>
2023-12-21 11:24:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+
+function App() {
+    
+    return (
+        <>
+            <Header />
+        <div className="container">  
+            <h1>My APP</h1>
+        </div>
+
+    </>
+    )
+}
+
+export default App
